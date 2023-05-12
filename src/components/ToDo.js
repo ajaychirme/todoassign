813f6f741d066a64d8b4f4fbda5e2a0d99017d79
ajaychirme <ajaychirme84@gmail.com>
2023-05-12 16:37:22 +0530

    commiting frontend

---
+++ src/components/ToDo.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import {BiEdit} from "react-icons/bi"
+import {AiFillDelete} from "react-icons/ai"
+
+const ToDo = ({text, updateMode, deleteToDo}) => {
+    return (
+        <div className="todo">
+            <div className="text">{text}</div>
+            <div className="icons">
+                <BiEdit className='icon-upd' onClick={updateMode} />
+                <AiFillDelete className='icon-del' onClick={deleteToDo} />
+            </div>
+        </div>
+    )
+}
+
+export default ToDo
